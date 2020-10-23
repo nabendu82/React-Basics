d497ac537faa0eb8e26a403e63ee1ed205c39505
Nabendu <nabendu.biswas@gmail.com>
2020-10-23 12:15:42 +0530

    Part-17 complete

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import './App.css';
-import ClickCounter2 from './components/ClickCounter2';
-import CounterRender from './components/CounterRender';
-import HoverCounter2 from './components/HoverCounter2';
+import GrandParent from './components/GrandParent';
+import { UserProvider } from './components/userContext';
 
 function App() {
   return (
     <div className="App">
-      <CounterRender>
-        {(count, incrementCount) => (
-          <ClickCounter2 count={count} incrementCount={incrementCount} />
-        )}
-      </CounterRender>
-
-      <CounterRender>
-        {(count, incrementCount) => (
-          <HoverCounter2 count={count} incrementCount={incrementCount} />
-        )}
-      </CounterRender>
+      <UserProvider value="Nabendu" >
+        <GrandParent />
+      </UserProvider>
     </div>
   );
 }
